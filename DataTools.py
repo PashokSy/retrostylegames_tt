94643687b8d05c3610bb31e8f0cb0ba274856224
PavelsPC <pavlo.syzonenko@gmail.com>
2023-04-22 20:38:55 +0300

    Create DataTools.py

---
+++ DataTools.py
@@ -0,0 +1,25 @@
+import csv
+
+import pandas as pd
+
+
+def writeDataSetInCSV(data_set):
+    game = pd.DataFrame.from_dict(data_set, orient='index')
+    games = game.transpose()
+    games.to_csv('games_list.csv', index=False, header=True, mode='w')
+
+
+def readDataSetInCSV():
+    data_set = {'name': [], 'date': [], 'platform': [], 'score': [], 'reviewCount': [], 'style': [], 'developer': []}
+    filename = 'games_list.csv'
+    with open(filename, 'r') as data:
+        for line in csv.reader(data):
+            if line[0] == 'name':
+                continue
+            data_set['name'] = line[0]
+            data_set['date'] = line[1]
+            data_set['platform'] = line[2]
+            data_set['score'] = line[3]
+            data_set['reviewCount'] = line[4]
+            data_set['style'] = line[5]
+            data_set['developer'] = line[6]
