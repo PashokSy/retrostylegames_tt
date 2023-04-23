e31bdedbefdb32fa7ec08df50a9947639de2f310
PavelsPC <pavlo.syzonenko@gmail.com>
2023-04-23 18:15:31 +0300

    finish scrapers and data gather

--- DataTools.py
+++ DataTools.py
@@ -1,4 +1,6 @@
 import csv
+import itertools
+
 import pandas as pd
 
 
@@ -8,10 +10,10 @@ def writeDataSetInCSV(data_set, file_name):
     games.to_csv(str(file_name), index=False, header=True, mode='w+')
 
 
-def readDataSetInCSV(file_name):
+def readDataSetFromCSV(file_name):
     data_set = {'name': [], 'date': [], 'platform': [], 'score': [], 'reviewCount': [],
                 'style': [], 'developer': [], 'downloads': []}
-    with open(str(file_name), 'r') as data:
+    with open(str(file_name), 'r+') as data:
         for line in csv.reader(data):
             if line[0] == 'name':
                 continue
@@ -24,3 +26,17 @@ def readDataSetInCSV(file_name):
             data_set['developer'].append(line[6])
             data_set['downloads'].append(line[7])
     return data_set
+
+
+def concatTwoDict(dict1, dict2):
+    for key in dict2:
+        for value in dict2[key]:
+            dict1[key].append(value)
+    return dict1
+
+
+def key_value_combinations(keys, values):
+    items = list(zip(keys, values))
+    value_combinations = itertools.product(*[item[1] for item in items])
+    combinations = [{items[i][0]: combination[i] for i in range(len(items))} for combination in value_combinations]
+    return combinations
--- Main.py
+++ Main.py
@@ -1,18 +1,26 @@
+import json
+import pprint
+import time
+
+import numpy as np
+
 import MetacriticScrapper as metacriticScrapper
 import GooglePlayScrapper as googlePlayScrapper
 import DataTools as dataTools
 
-game_list = {'name': [], 'date': [], 'platform': [], 'score': [], 'reviewCount': [],
-                'style': [], 'developer': [], 'downloads': []}
+
 def Main():
+    # scrap and write metacritic data
     # metacritic_data = metacriticScrapper.scrapDataFromMetacritic(2)
-    # google_data = googlePlayScrapper.scrapDataFromGooglePlay()
     # dataTools.writeDataSetInCSV(metacritic_data, 'metacritic_data.csv')
+    # scrap and write google data
+    # google_data = googlePlayScrapper.scrapDataFromGooglePlay()
     # dataTools.writeDataSetInCSV(google_data, 'google_data.csv')
 
-    game_list = dataTools.readDataSetInCSV('google_data.csv')
-
-    print(game_list.values())
-
+    # concat two dictionaries and write result into csv file
+    metacritic_list = dataTools.readDataSetFromCSV('metacritic_data.csv')
+    google_list = dataTools.readDataSetFromCSV('google_data.csv')
+    result_list = dataTools.concatTwoDict(metacritic_list, google_list)
+    # dataTools.writeDataSetInCSV(result_list, 'games_list.csv')
 
 Main()
